691b92de492394beb706dbf19f6cd0a7ee4e89bb
Jacob Bayisa <bayisagobu@gmail.com>
2021-05-08 17:13:29 -0400

    Toggle effect added to task reminder list

--- src/App.js
+++ src/App.js
@@ -30,12 +30,20 @@ function App() {
   const deleteTask = (id) => {
     
       setTasks(tasks.filter((task) => task.id !== id))
-    
+  }
+
+  // Toggle reminder for left border 
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder} : task
+      )
+    )
   }
   return (
     <div className="container">
       <Header />
-      <Tasks tasks = {tasks} onDelete = {deleteTask} />
+      { tasks.length > 0 ? <Tasks tasks = {tasks} onDelete = {deleteTask}  onToggle ={toggleReminder}/> : " No list to show"}
     </div>
   );
 }
